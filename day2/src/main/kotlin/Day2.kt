ae68c82004a60a70a92a62905b48a5b0b87fb026
mrowkaadrian <mrowkaadrian@gmail.com>
2024-12-02 12:48:16 +0100

    Added Day2 solution

--- day2/src/main/kotlin/Day2.kt
+++ day2/src/main/kotlin/Day2.kt
@@ -1,19 +1,60 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
 fun main() {
     println(part1())
     println(part2())
 }
 
 fun part1(): Any {
-    return input
+    val matrix = readFileAsMatrix("Day2.input")
+    var result = 0
 
+    matrix.forEach {
+        if (analyseLine(it))
+            result++
+    }
+
+    return result
 }
 
 fun part2(): Any {
-    return input.reversed()
+    val matrix = readFileAsMatrix("Day2.input")
+    var result = 0
+
+    matrix.forEach {
+        if (analyseLineWithTolerance(it))
+            result++
+    }
+    return result
 }
 
-private val input: String by lazy {readInput()}
+private fun readFileAsMatrix(fileName: String) : List<List<Int>> {
+    val resource = object {}.javaClass.getResource(fileName)
+        ?: throw IllegalArgumentException("File $fileName not found in resources.")
+    return Files.readAllLines(Paths.get(resource.toURI()))
+        .map { line ->
+            line.trim().split(" ").map(String::toInt)
+        }
+}
 
-private fun readInput(): String {
-    return object {}.javaClass.getResource("Day2.input").readText()
+private fun analyseLine(numbers: List<Int>) : Boolean {
+
+    val noDuplicates = numbers.distinct().size == numbers.size
+    val noBigGaps = numbers.zipWithNext { a, b -> (-3 ..3).contains(b - a) }.all { it }
+    val isIncreasing = numbers.zipWithNext { a, b -> b > a }.all { it }
+    val isDecreasing = numbers.zipWithNext { a, b -> b < a }.all { it }
+
+    return noDuplicates && noBigGaps && (isIncreasing || isDecreasing)
+}
+
+private fun analyseLineWithTolerance(numbers: List<Int>) : Boolean {
+    if (analyseLine(numbers))
+        return true
+
+    // Try removing each element and check if the resulting list is valid
+    return numbers.indices.any { index ->
+        val modifiedList = numbers.toMutableList().apply { removeAt(index) }
+        analyseLine(modifiedList)
+    }
 }
