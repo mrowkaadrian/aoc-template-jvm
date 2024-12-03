6605118b0de1222e52c608a11dc9bb91d46bed5c
mrowkaadrian <mrowkaadrian@gmail.com>
2024-12-03 15:00:58 +0100

    Added Day3 solution

--- day3/src/main/kotlin/Day3.kt
+++ day3/src/main/kotlin/Day3.kt
@@ -4,12 +4,42 @@ fun main() {
 }
 
 fun part1(): Any {
-    return input
+    var result = 0
+
+    Regex("mul\\(\\d+,\\d+\\)").findAll(input).forEach {
+        result += executeMultiplication(it)
+    }
+
+    return result
 
 }
 
 fun part2(): Any {
-    return input.reversed()
+    var result = 0
+    var isEnabled = true
+
+    Regex("(do\\(\\)|don't\\(\\)|mul\\(\\d+,\\d+\\))")
+        .findAll(input)
+        .forEach { operation ->
+            when {
+                operation.value == "do()" -> isEnabled = true
+                operation.value == "don't()" -> isEnabled = false
+                operation.value.startsWith("mul") ->  {
+                    if (isEnabled)
+                        result += executeMultiplication(operation)
+                }
+            }
+        }
+
+    return result
+}
+
+private fun executeMultiplication(it: MatchResult) : Int {
+    return it.value
+        .removeSurrounding("mul(", ")")
+        .split(",")
+        .map { it.toInt() }
+        .reduce { a, b -> a * b }
 }
 
 private val input: String by lazy {readInput()}
